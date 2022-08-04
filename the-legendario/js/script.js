c9bec506c2f49a63145a9f940c5f898ea457ad69
Alan Torino <alantorino42@gmail.com>
2022-08-04 10:34:12 -0300

    Primer commit

---
+++ the-legendario/js/script.js
@@ -0,0 +1,25 @@
+window.addEventListener('scroll', function () {
+
+const nav = document.querySelector('#nav-bar');
+var altura = this.window.innerHeight;
+var distancia = nav.getBoundingClientRect().top;
+
+
+if (distancia < (altura * 0.7)){
+    nav.classList.add('show-nav-bar')
+}
+
+else {
+    nav.classList.remove('show-nav-bar')
+}
+
+console.log(altura, distancia)
+
+  })
+  
+
+
+const typed = new Typed('.typed', {
+    strings: ["<i class='second-p-js'>Lorem ipsum dolor sit amet consectetur adipisicing elit Reiciendis corrupti eum, illo distinctio impedit, sed harum ratione asperiores, molestiae dolorum hic quas officiis quia! Saepe et velit laudantium minus! Quia assumenda ratione labore porro aliquid reiciendis ipsum, sapiente tempora magnam similique laborum repellendus commodi autem, quisquam officiis aperiam earum, expedita animi odit? Harum cum expedita odio molestias dolorum molestiae quia veniam veritatis?.</i>"]
+
+})
