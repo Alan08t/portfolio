86ed7e3dd20b046c65df64ede5802684fc98a6c1
Alan Torino <alantorino42@gmail.com>
2022-08-06 22:44:19 -0300

    Segundo commit

--- the-legendario/js/script.js
+++ the-legendario/js/script.js
@@ -1,11 +1,14 @@
-window.addEventListener('scroll', function () {
+{
+    
+    
+    window.addEventListener('scroll', function () {
 
-const nav = document.querySelector('#nav-bar');
+const nav = document.querySelector('.nav-bar');
 var altura = this.window.innerHeight;
 var distancia = nav.getBoundingClientRect().top;
 
 
-if (distancia < (altura * 0.7)){
+if (distancia < altura){
     nav.classList.add('show-nav-bar')
 }
 
@@ -16,10 +19,27 @@ else {
 console.log(altura, distancia)
 
   })
+}
+
+{
   
 
 
 const typed = new Typed('.typed', {
     strings: ["<i class='second-p-js'>Lorem ipsum dolor sit amet consectetur adipisicing elit Reiciendis corrupti eum, illo distinctio impedit, sed harum ratione asperiores, molestiae dolorum hic quas officiis quia! Saepe et velit laudantium minus! Quia assumenda ratione labore porro aliquid reiciendis ipsum, sapiente tempora magnam similique laborum repellendus commodi autem, quisquam officiis aperiam earum, expedita animi odit? Harum cum expedita odio molestias dolorum molestiae quia veniam veritatis?.</i>"]
 
+});
+
+}
+
+{
+
+
+let navMenu = document.querySelector(".nav-menu");
+
+document.querySelectorAll('.btn-menu').forEach(item => {
+    item.addEventListener('click', event => {
+        navMenu.classList.toggle("nav-menu-visible");
+    })
 })
+}
