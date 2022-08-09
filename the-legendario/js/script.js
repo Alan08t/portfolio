c1fc0b787edbd82311f212cd420f883036a8cd66
Alan Torino <alantorino42@gmail.com>
2022-08-08 21:39:14 -0300

    Tercer commit

--- the-legendario/js/script.js
+++ the-legendario/js/script.js
@@ -21,25 +21,23 @@ console.log(altura, distancia)
   })
 }
 
-{
-  
 
-
-const typed = new Typed('.typed', {
-    strings: ["<i class='second-p-js'>Lorem ipsum dolor sit amet consectetur adipisicing elit Reiciendis corrupti eum, illo distinctio impedit, sed harum ratione asperiores, molestiae dolorum hic quas officiis quia! Saepe et velit laudantium minus! Quia assumenda ratione labore porro aliquid reiciendis ipsum, sapiente tempora magnam similique laborum repellendus commodi autem, quisquam officiis aperiam earum, expedita animi odit? Harum cum expedita odio molestias dolorum molestiae quia veniam veritatis?.</i>"]
-
-});
-
-}
 
 {
 
 
-let navMenu = document.querySelector(".nav-menu");
+    const btnMenu = document.querySelector('.btn-menu');
+    const navMenu = document.querySelector('.nav-menu');
+    const navBar = document.querySelector('.nav-bar')
 
-document.querySelectorAll('.btn-menu').forEach(item => {
-    item.addEventListener('click', event => {
-        navMenu.classList.toggle("nav-menu-visible");
-    })
-})
+
+    btnMenu.addEventListener('click', toggleMenu);
+    
+
+    function toggleMenu() {
+            navMenu.classList.toggle('nav-menu-visible');
+            navBar.classList.toggle('nav-bar-desplegate')
+
+
+    }
 }
